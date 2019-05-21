4d3d51479dcd526fe5b1f6f89e7c7c19cae7e5c3
jodhka4 <jodhka4@gmail.com>
2019-05-20 23:10:10 -0400

    Add build files

---
+++ Debug/Src/subdir.mk
@@ -0,0 +1,46 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Src/LSM9DS1.c \
+../Src/glove_status_codes.c \
+../Src/main.c \
+../Src/sm.c \
+../Src/stm32l4xx_hal_msp.c \
+../Src/stm32l4xx_it.c \
+../Src/syscalls.c \
+../Src/system_stm32l4xx.c 
+
+OBJS += \
+./Src/LSM9DS1.o \
+./Src/glove_status_codes.o \
+./Src/main.o \
+./Src/sm.o \
+./Src/stm32l4xx_hal_msp.o \
+./Src/stm32l4xx_it.o \
+./Src/syscalls.o \
+./Src/system_stm32l4xx.o 
+
+C_DEPS += \
+./Src/LSM9DS1.d \
+./Src/glove_status_codes.d \
+./Src/main.d \
+./Src/sm.d \
+./Src/stm32l4xx_hal_msp.d \
+./Src/stm32l4xx_it.d \
+./Src/syscalls.d \
+./Src/system_stm32l4xx.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Src/%.o: ../Src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo $(PWD)
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 '-D__weak=__attribute__((weak))' '-D__packed="__attribute__((__packed__))"' -DUSE_HAL_DRIVER -DSTM32L452xx -I"/Users/tejasvi/FydpProto/Inc" -I"/Users/tejasvi/FydpProto/Drivers/STM32L4xx_HAL_Driver/Inc" -I"/Users/tejasvi/FydpProto/Drivers/STM32L4xx_HAL_Driver/Inc/Legacy" -I"/Users/tejasvi/FydpProto/Drivers/CMSIS/Device/ST/STM32L4xx/Include" -I"/Users/tejasvi/FydpProto/Drivers/CMSIS/Include"  -Og -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
